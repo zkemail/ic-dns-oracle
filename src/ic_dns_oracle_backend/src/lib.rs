1bd2230d2224c8ef74535118c1872df6dc83e702
SoraSuegami <suegamisora@gmail.com>
2023-11-12 04:10:19 +0900

    Add test for private to public.

--- src/ic_dns_oracle_backend/src/lib.rs
+++ src/ic_dns_oracle_backend/src/lib.rs
@@ -15,7 +15,10 @@ use ic_evm_sign::state::Environment;
 use ic_storage::IcStorage;
 use regex::Regex;
 use rsa::{
-    pkcs1::DecodeRsaPublicKey, pkcs8::DecodePublicKey, traits::PublicKeyParts, RsaPublicKey,
+    pkcs1::{DecodeRsaPrivateKey, DecodeRsaPublicKey},
+    pkcs8::DecodePublicKey,
+    traits::{PrivateKeyParts, PublicKeyParts},
+    BigUint, RsaPrivateKey, RsaPublicKey,
 };
 use serde::{Deserialize, Serialize};
 use serde_json::{self, Value};
@@ -34,10 +37,22 @@ pub struct SignedDkimPublicKey {
     pub selector: String,
     pub domain: String,
     pub chain_id: u64,
-    pub timestamp: u64,
+    // pub timestamp: u64,
     pub signature: String,
     pub public_key: String,
     pub public_key_hash: String,
+}
+
+#[derive(Default, CandidType, Deserialize, Debug, Clone)]
+pub struct SignedRevocation {
+    pub selector: String,
+    pub domain: String,
+    pub chain_id: u64,
+    // pub timestamp: u64,
+    pub signature: String,
+    pub public_key: String,
+    pub public_key_hash: String,
+    pub private_key: String,
 }
 
 #[derive(Default, IcStorage, Debug)]
@@ -227,12 +242,12 @@ pub async fn sign_dkim_public_key(
     })?;
     info!("cycle {}", ic_cdk::api::call::msg_cycles_available128());
     let public_key_hash_hex = res?;
-    let timestamp = ic_cdk::api::time();
-    let timestamp_sec = timestamp / 1_000_000_000;
-    info!("timestamp {}", timestamp_sec);
+    // let timestamp = ic_cdk::api::time();
+    // let timestamp_sec = timestamp / 1_000_000_000;
+    // info!("timestamp {}", timestamp_sec);
     let message = format!(
-        "chain_id={};selector={};domain={};timestamp={};public_key_hash={};",
-        canister_state.chain_id, selector, domain, timestamp_sec, public_key_hash_hex
+        "SET:chain_id={};selector={};domain={};public_key_hash={};",
+        canister_state.chain_id, selector, domain, public_key_hash_hex
     );
     let signature = sign(message).await?;
 
@@ -240,7 +255,7 @@ pub async fn sign_dkim_public_key(
         selector,
         domain: domain.clone(),
         chain_id: canister_state.chain_id,
-        timestamp: timestamp_sec,
+        // timestamp: timestamp_sec,
         signature,
         public_key,
         public_key_hash: public_key_hash_hex,
@@ -251,10 +266,97 @@ pub async fn sign_dkim_public_key(
     Ok(res)
 }
 
+#[ic_cdk::update]
+pub async fn revoke_dkim_public_key(
+    selector: String,
+    domain: String,
+    private_key_der: String,
+) -> Result<SignedRevocation, String> {
+    let available_cycles = ic_cdk::api::call::msg_cycles_available128();
+    let accepted_cycles = ic_cdk::api::call::msg_cycles_accept128(available_cycles);
+    if available_cycles != accepted_cycles {
+        return Err("not all of the available_cycles is moved".to_string());
+    }
+    info!("available_cycles {}", available_cycles);
+    let canister_state = CanisterState::get();
+    info!("state {:?}", canister_state.clone());
+    let canister_state = canister_state.borrow_mut();
+    if available_cycles < canister_state.fee_cycles {
+        return Err("not enough cycles".to_string());
+    }
+    if canister_state.address == "" {
+        return Err("ethereum address is not initialized".to_string());
+    }
+    let revoked_public_key = private_to_public_key(&private_key_der.as_bytes());
+    info!("public_key {}", revoked_public_key);
+    let fetched_public_key =
+        get_dkim_public_key(&selector, &domain, canister_state.http_cycles).await?;
+    info!("fetched_public_key {}", fetched_public_key);
+    if revoked_public_key != fetched_public_key {
+        return Err(
+            "The given private key does not correspond to the fetched public key".to_string(),
+        );
+    }
+    if canister_state.poseidon_canister_id == "" {
+        return Err("poseidon_canister_id unknown".to_string());
+    }
+    info!(
+        "poseidon_canister_id {}",
+        canister_state.poseidon_canister_id
+    );
+    let poseidon_canister_id =
+        Principal::from_text(canister_state.poseidon_canister_id.clone()).unwrap();
+    info!("cycle {}", ic_cdk::api::call::msg_cycles_available128());
+    let (res,): (Result<String, String>,) = ic_cdk::api::call::call_with_payment128(
+        poseidon_canister_id,
+        "public_key_hash",
+        (fetched_public_key.clone(),),
+        canister_state.poseidon_cycles,
+    )
+    .await
+    .map_err(|(code, e)| {
+        format!(
+            "calling poseidon canister failed. code: {:?}, reason: {}",
+            code, e
+        )
+    })?;
+    info!("cycle {}", ic_cdk::api::call::msg_cycles_available128());
+    let public_key_hash_hex = res?;
+    let message = format!(
+        "REVOKE:chain_id={};selector={};domain={};public_key_hash={};",
+        canister_state.chain_id, selector, domain, public_key_hash_hex
+    );
+    let signature = sign(message).await?;
+
+    let res = SignedRevocation {
+        selector,
+        domain: domain.clone(),
+        chain_id: canister_state.chain_id,
+        signature,
+        public_key: fetched_public_key,
+        public_key_hash: public_key_hash_hex,
+        private_key: private_key_der,
+    };
+    // let mut domain_state = canister_state.domain_states.get_mut(&domain).unwrap();
+    // domain_state.previous_response = Some(res.clone());
+    info!("cycle {}", ic_cdk::api::call::msg_cycles_available128());
+    Ok(res)
+}
+
 async fn sign(message: String) -> Result<String, String> {
     let signature =
         ic_evm_sign::sign_msg(message.as_bytes().to_vec(), Principal::anonymous()).await?;
     Ok(signature)
+}
+
+fn private_to_public_key(private_key_der: &[u8]) -> String {
+    let private_key = RsaPrivateKey::from_pkcs1_der(private_key_der)
+        .expect("Given private key is invalid format.");
+    let public_key = private_key.to_public_key();
+    if public_key.e() != &BigUint::from(65537u64) {
+        panic!("Only the fixed e parameter is supported.");
+    }
+    "0x".to_string() + &hex::encode(&public_key.n().to_bytes_be())
 }
 
 #[cfg(test)]
@@ -266,19 +368,21 @@ mod test {
     use ethers_core::types::*;
     use hex;
     use poseidon::public_key_hash;
+    use rsa::pkcs1::{der::SecretDocument, EncodeRsaPrivateKey};
+    use rsa::rand_core::OsRng;
 
     #[test]
     fn test_sign_dkim_public_key() {
         // The following values are obtained by running the canister locally.
         let selector = "20230601";
         let domain = "gmail.com";
-        let timestamp = 1_697_875_531;
+        // let timestamp = 1_697_875_531;
         let public_key = "0x9edbd2293d6192a84a7b4c5c699d31f906e8b83b09b817dbcbf4bcda3c6ca02fd2a1d99f995b360f52801f79a2d40a9d31d535da1d957c44de389920198ab996377df7a009eee7764b238b42696168d1c7ecbc7e31d69bf3fcc337549dc4f0110e070cec0b111021f0435e51db415a2940011aee0d4db4767c32a76308aae634320642d63fe2e018e81f505e13e0765bd8f6366d0b443fa41ea8eb5c5b8aebb07db82fb5e10fe1d265bd61b22b6b13454f6e1273c43c08e0917cd795cc9d25636606145cff02c48d58d0538d96ab50620b28ad9f5aa685b528f41ef1bad24a546c8bdb1707fb6ee7a2e61bbb440cd9ab6795d4c106145000c13aeeedd678b05f";
         let pk_hash = "0x0ea9c777dc7110e5a9e89b13f0cfc540e3845ba120b2b6dc24024d61488d4788";
         assert_eq!(public_key_hash(public_key.to_string()).unwrap(), pk_hash);
         let expected_msg = format!(
-            "chain_id={};selector={};domain={};timestamp={};public_key_hash={};",
-            1, selector, domain, timestamp, pk_hash
+            "SET:chain_id={};selector={};domain={};public_key_hash={};",
+            1, selector, domain, pk_hash
         );
         println!("expected_msg {}", expected_msg);
         let len = expected_msg.len();
@@ -294,6 +398,19 @@ mod test {
         assert_eq!(
             recovered,
             H160::from_slice(&hex::decode("1c63df16d1212ecf5d497984dfd1aa23904756ff").unwrap())
+        );
+    }
+
+    #[test]
+    fn test_private_to_public() {
+        let private_key = RsaPrivateKey::new(&mut OsRng, 2048).unwrap();
+        let public_key = private_key.to_public_key();
+        let private_key_der = private_key.to_pkcs1_der().unwrap();
+
+        let public_key_hex = private_to_public_key(private_key_der.as_bytes());
+        assert_eq!(
+            public_key_hex,
+            "0x".to_string() + &hex::encode(&public_key.n().to_bytes_be())
         );
     }
 }
